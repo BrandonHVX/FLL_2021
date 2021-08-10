57f1ff6ec52bed110d3b102b670a9308839c0083
Brandon <brandongines@gmail.com>
2021-08-10 07:43:54 -0400

    form-upated

--- src/components/sections/home/Form.1.js
+++ src/components/sections/home/Form.1.js
@@ -1,120 +1,120 @@
-import React from "react";
-import { useState, useEffect } from "react";
+// import React from "react";
+// import { useState, useEffect } from "react";
 
-const Form = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: ""
-  });
+// const Form = () => {
+//   const [formData, setFormData] = useState({
+//     name: "",
+//     email: "",
+//     message: ""
+//   });
 
-  const handleChange = e => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
+//   const handleChange = e => {
+//     const { name, value } = e.target;
+//     setFormData({
+//       ...formData,
+//       [name]: value
+//     });
+//   };
 
-  const [errors, setErrors] = useState({});
-  const validate = formData => {
-    let formErrors = {};
-    if (!formData.name) {
-      formErrors.name = "Name required";
-    }
-    if (!formData.email) {
-      formErrors.email = "Email required";
-    }
-    if (!formData.message) {
-      formErrors.message = "Message is required";
-    }
-    return formErrors;
-  };
+//   const [errors, setErrors] = useState({});
+//   const validate = formData => {
+//     let formErrors = {};
+//     if (!formData.name) {
+//       formErrors.name = "Name required";
+//     }
+//     if (!formData.email) {
+//       formErrors.email = "Email required";
+//     }
+//     if (!formData.message) {
+//       formErrors.message = "Message is required";
+//     }
+//     return formErrors;
+//   };
 
-  const [isSubmitted, setIsSubmitted] = useState(false);
+//   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const handleSubmit = e => {
-    setErrors(validate(formData));
-    setIsSubmitted(true);
-    e.preventDefault();
-  };
+//   const handleSubmit = e => {
+//     setErrors(validate(formData));
+//     setIsSubmitted(true);
+//     e.preventDefault();
+//   };
 
-  const encode = data => {
-    return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
-  };
+//   const encode = data => {
+//     return Object.keys(data)
+//       .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+//       .join("&");
+//   };
 
-  useEffect(() => {
-    if (Object.keys(errors).length === 0 && isSubmitted) {
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({ "form-name": "contact", ...formData })
-      })
-        .then(() => alert("Success!"))
-        .then(() => setIsSubmitted(false))
-        .then(() => setFormData({ name: "", email: "", message: "" }))
-        .catch(error => alert(error));
-    }
-  }, [errors, formData, isSubmitted]);
+//   useEffect(() => {
+//     if (Object.keys(errors).length === 0 && isSubmitted) {
+//       fetch("/", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//         body: encode({ "form-name": "contact", ...formData })
+//       })
+//         .then(() => alert("Success!"))
+//         .then(() => setIsSubmitted(false))
+//         .then(() => setFormData({ name: "", email: "", message: "" }))
+//         .catch(error => alert(error));
+//     }
+//   }, [errors, formData, isSubmitted]);
 
-  // console.log(errors, formData)
-  return (
-    <div className="contact-form">
-      <h1> Sample Form </h1>
-      <form
-        onSubmit={handleSubmit}
-        name="contact"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <div>
-          <label htmlFor="name" className="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            value={formData.name}
-            onChange={handleChange}
-          />
-          {errors.name && <p>{errors.name}</p>}
-        </div>
-        <div>
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-          {errors.email && <p>{errors.email}</p>}
-        </div>
-        <div className="form-inputs">
-          <label htmlFor="message" className="form-label">
-            Message
-          </label>
-          <textarea
-            name="message"
-            id="message"
-            value={formData.message}
-            onChange={handleChange}
-          />
-          {errors.message && <p>{errors.message}</p>}
-        </div>
-        <button type="submit" className="form-input-btn">
-          Send
-        </button>
-      </form>
-    </div>
-  );
-};
+//   // console.log(errors, formData)
+//   return (
+//     <div className="contact-form">
+//       <h1> Sample Form </h1>
+//       <form
+//         onSubmit={handleSubmit}
+//         name="contact"
+//         method="post"
+//         data-netlify="true"
+//         data-netlify-honeypot="bot-field"
+//       >
+//         <input type="hidden" name="form-name" value="contact" />
+//         <div>
+//           <label htmlFor="name" className="form-label">
+//             Name
+//           </label>
+//           <input
+//             type="text"
+//             name="name"
+//             id="name"
+//             value={formData.name}
+//             onChange={handleChange}
+//           />
+//           {errors.name && <p>{errors.name}</p>}
+//         </div>
+//         <div>
+//           <label htmlFor="email" className="form-label">
+//             Email
+//           </label>
+//           <input
+//             type="email"
+//             name="email"
+//             id="email"
+//             value={formData.email}
+//             onChange={handleChange}
+//           />
+//           {errors.email && <p>{errors.email}</p>}
+//         </div>
+//         <div className="form-inputs">
+//           <label htmlFor="message" className="form-label">
+//             Message
+//           </label>
+//           <textarea
+//             name="message"
+//             id="message"
+//             value={formData.message}
+//             onChange={handleChange}
+//           />
+//           {errors.message && <p>{errors.message}</p>}
+//         </div>
+//         <button type="submit" className="form-input-btn">
+//           Send
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
 
-export default Form;
+// export default Form;
--- src/components/sections/home/Form.js
+++ src/components/sections/home/Form.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { useState, useEffect } from "react";
+// you will need the css that comes with bootstrap@3. if you are using
+// a tool like webpack, you can do the following:
+
+// you will also need the css that comes with bootstrap-daterangepicker
 
 const Form = () => {
   const [formData, setFormData] = useState({
@@ -15,7 +19,27 @@ const Form = () => {
     state: "",
     zip: "",
     phone: "",
-    message: ""
+    message: "",
+    school: "",
+    diploma: "",
+    employer: "",
+    employersupervisor: "",
+    employerphone: "",
+    startdate: "",
+    enddate: "",
+    jobduties: "",
+    jobexp: "",
+    cpuexp: "",
+    manageexp: "",
+    accidents: "",
+    accidentsexp: "",
+    violations: "",
+    violationsexp: "",
+    refname: "",
+    refphone: "",
+    refemail: "",
+    signature: "",
+    date: ""
   });
 
   const handleChange = e => {
@@ -78,7 +102,27 @@ const Form = () => {
             state: "",
             zip: "",
             phone: "",
-            message: ""
+            message: "",
+            school: "",
+            diploma: "",
+            employer: "",
+            employersupervisor: "",
+            employerphone: "",
+            startdate: "",
+            enddate: "",
+            jobduties: "",
+            jobexp: "",
+            cpuexp: "",
+            manageexp: "",
+            accidents: "",
+            accidentsexp: "",
+            violations: "",
+            violationsexp: "",
+            refname: "",
+            refphone: "",
+            refemail: "",
+            signature: "",
+            date: ""
           })
         )
         .catch(error => alert(error));
@@ -89,9 +133,9 @@ const Form = () => {
   return (
     <section className="" id="contact">
       <div className="container">
-        <div className=" grey-bg">
-          <div className="">
-            <h1> Sample Form </h1>
+        <div className=" grey-bg  ">
+          <div className="m-2">
+            <h6> Personal Information</h6>
             <form
               onSubmit={handleSubmit}
               name="contact"
@@ -101,7 +145,7 @@ const Form = () => {
               data-netlify-honeypot="bot-field"
             >
               <input type="hidden" name="form-name" value="contact" />
-              <div className="col-md-3">
+              <div className="col-md-3  ">
                 <label htmlFor="firstname" className="form-label">
                   First Name
                 </label>
@@ -114,7 +158,7 @@ const Form = () => {
                 />
                 {errors.firstname && <p>{errors.firstname}</p>}
               </div>
-              <div className="col-md-3">
+              <div className="col-md-4">
                 <label htmlFor="lastname" className="form-label">
                   Last Name
                 </label>
@@ -131,17 +175,23 @@ const Form = () => {
                 <label htmlFor="sex" className="form-label">
                   Sex
                 </label>
-                <input
+                <select
                   type="text"
                   name="sex"
                   id="sex"
                   value={formData.sex}
                   onChange={handleChange}
-                />
+                  class="form-select"
+                  id="inputGroupSelect01"
+                >
+                  <option selected>Select</option>
+                  <option value="1">Male</option>
+                  <option value="2">Female</option>
+                </select>
                 {errors.sex && <p>{errors.sex}</p>}
               </div>
-              <div className="col-md-4">
-                <label htmlFor="dob" className="form-label">
+              <div className="col-md-3">
+                <label htmlFor="social" className="form-label">
                   Date Of Birth
                 </label>
                 <input
@@ -256,6 +306,193 @@ const Form = () => {
                   onChange={handleChange}
                 />
                 {errors.zip && <p>{errors.zip}</p>}
+              </div>
+              {/* /////// */}
+              <div className="row mt-5">
+                <h6> Education</h6>
+              </div>
+              <div className="col-md-3">
+                <label htmlFor="firstname" className="form-label">
+                  School
+                </label>
+                <input
+                  type="text"
+                  name="school"
+                  id="school"
+                  value={formData.school}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-md-2">
+                <label htmlFor="sex" className="form-label">
+                  Diploma
+                </label>
+                <input
+                  type="text"
+                  name="diploma"
+                  id="diploma"
+                  value={formData.diploma}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="row mt-5">
+                <h6> Employment History</h6>
+              </div>
+              <div className="col-md-2">
+                <label htmlFor="sex" className="form-label">
+                  Employer
+                </label>
+                <input
+                  type="text"
+                  name="employer"
+                  id="employer"
+                  value={formData.employer}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-md-4">
+                <label htmlFor="dob" className="form-label">
+                  employersupervisor
+                </label>
+                <input
+                  type="text"
+                  name="employersupervisor"
+                  id="employersupervisor"
+                  value={formData.employersupervisor}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-md-4">
+                <label htmlFor="social" className="form-label">
+                  employerphone
+                </label>
+                <input
+                  type="text"
+                  name="employerphone"
+                  id="employerphone"
+                  value={formData.employerphone}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-md-4">
+                <label htmlFor="email" className="form-label">
+                  startdate
+                </label>
+                <input
+                  type="text"
+                  name="startdate"
+                  id="startdate"
+                  value={formData.startdate}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-md-4">
+                <label htmlFor="email" className="form-label">
+                  enddate
+                </label>
+                <input
+                  type="text"
+                  name="enddate"
+                  id="enddate"
+                  value={formData.enddate}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-md-10">
+                <label htmlFor="email" className="form-label">
+                  jobduties
+                </label>
+                <input
+                  type="text"
+                  name="jobduties"
+                  id="jobduties"
+                  value={formData.jobduties}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-md-2">
+                <label htmlFor="email" className="form-label">
+                  jobexp
+                </label>
+                <input
+                  type="text"
+                  name="jobexp"
+                  id="jobexp"
+                  value={formData.jobexp}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-md-4">
+                <label htmlFor="email" className="form-label">
+                  cpuexp
+                </label>
+                <input
+                  type="text"
+                  name="cpuexp"
+                  id="cpuexp"
+                  value={formData.cpuexp}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-md-4">
+                <label htmlFor="email" className="form-label">
+                  manageexp
+                </label>
+                <input
+                  type="text"
+                  name="manageexp"
+                  id="manageexp"
+                  value={formData.manageexp}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-md-4">
+                <label htmlFor="email" className="form-label">
+                  accidents
+                </label>
+                <input
+                  type="text"
+                  name="accidents"
+                  id="accidents"
+                  value={formData.accidents}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-md-4">
+                <label htmlFor="email" className="form-label">
+                  accidentsexp
+                </label>
+                <input
+                  type="text"
+                  name="accidentsexp"
+                  id="accidentsexp"
+                  value={formData.accidentsexp}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-md-4">
+                <label htmlFor="email" className="form-label">
+                  violations
+                </label>
+                <input
+                  type="text"
+                  name="violations"
+                  id="violations"
+                  value={formData.violations}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-md-4">
+                <label htmlFor="email" className="form-label">
+                  violationsexp
+                </label>
+                <input
+                  type="text"
+                  name="violationsexp"
+                  id="violationsexp"
+                  value={formData.violationsexp}
+                  onChange={handleChange}
+                />
               </div>
 
               <button type="submit" className="form-input-btn">
